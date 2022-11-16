d6d53670bcdddfefa3b50fbb24a4e8f749f152ac
marcocrupi <marcocrupi@hotmail.it>
2022-11-16 07:07:58 +0100

    add lesson 29 and extra http request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,11 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
 import { HighlightDirective } from './direttive/highlight/highlight.directive';
+
 import { AboutComponent } from './componenti/about/about.component';
 import { ContactComponent } from './componenti/contact/contact.component';
 import { HomeComponent } from './componenti/home/home.component';
@@ -40,6 +44,8 @@ import { NotFoundComponent } from './componenti/not-found/not-found.component';
     MatButtonModule,
     MatInputModule,
     FormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
